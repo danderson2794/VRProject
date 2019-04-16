44e2b3607f961bf9b202aad00747f3ef5744889a
root <root@DESKTOP-N2K98DB.localdomain>
2019-04-15 22:44:59 -0400

    finished garrett's portion

---
+++ Assets/ReloadOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadOnClick : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
