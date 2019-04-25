26ae0c6b61c3241db9d836530cb1be357c308b91
root <root@DESKTOP-N2K98DB.localdomain>
2019-04-24 21:40:19 -0400

    stable working version before Nick's integration

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject arrowExplosion;
+    public GameObject bulletExplosion;
+    public GameObject projectileExplosion;
+
+    void onTriggerEnter(Collider other)
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Arrow")
+        {
+            Instantiate(arrowExplosion, other.transform.position, other.transform.rotation);
+        }
+        else if (other.tag == "Bullet")
+        {
+                Instantiate(bulletExplosion, other.transform.position, other.transform.rotation);
+                }
+        else if (other.tag == "Projectile")
+        {
+            Instantiate(projectileExplosion, other.transform.position, other.transform.rotation);
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/scoreTracking.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreTracking : MonoBehaviour
+{
+    bool initialized = false;
+    Vector3 lastPos = GameObject.Find("cube1").transform.position;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 curPos = GameObject.Find("cube1").transform.position;
+        if (curPos != lastPos)
+        {
+            print("Moving ");
+        }
+        lastPos = curPos;
+    }
+}
